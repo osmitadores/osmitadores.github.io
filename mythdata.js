9015c5bd5135c282417f162bb089019528eec18a
LucasFlicky <lucasflicky@hotmail.com>
2015-06-11 10:09:21 -0300

    Update mythdata.js

--- mythdata.js
+++ mythdata.js
@@ -1,44 +1,43 @@
-
 /*
 *   BADGES
 */
     var PATH = 'http://os-mitadores.lucasflicky.com/files/theme/BADGES/';
 
 //Tier
-    var mythUnicorn = createBadge('Mitador Unicorn: Raros porÃ©m participativos', 'uni');
+    var mythUnicorn = createBadge('Mitador Unicorn: Raros porém participativos', 'uni');
     var mythPlatinum = createBadge('Mitador Platinum', 'plat');
     var mythSilver = createBadge('Mitador Silver', 'silver');
-    var mythLegacy = createBadge('Mitador Legacy: Veterano dos primÃ³rdios!', 'legacy');
+    var mythLegacy = createBadge('Mitador Legacy: Veterano dos primórdios!', 'legacy');
     var mythFresh = createBadge('Freshman: Mitadores Trainee', 'fresh');
-    var mythGuest = createBadge('Guest: NÃ£o-mitador que convive na casa.', 'guest');
+    var mythGuest = createBadge('Guest: Não-mitador que convive na casa.', 'guest');
     var mythHonorary = createBadge('Mitador Honorary: Protegido pessoal de um Prime ou Platinum', 'honor');
     var mythLurker = createBadge('Lurker: Sempre nas sombras, eventualmente se pronuncia.', 'lurk');
     var mythGold = createBadge('Mitador Gold', 'gold');
-    var mythGhost = createBadge('Mitador Ghost: Inativo hÃ¡ mais de um mÃªs.', 'ghost');
+    var mythGhost = createBadge('Mitador Ghost: Inativo há mais de um mês.', 'ghost');
     var mythPrime = createBadge('Mitador Prime: Os primeiros Mitadores, criadores da porra toda.', 'prime');
 
 
 
-    var tierUnicorn   =   ["Unicorn",mythUnicorn,5];
-    var tierPlatinum  =   ["Platinum",mythPlatinum,2];
-    var tierSilver    =   ["Silver",mythSilver,4];
-    var tierLegacy    =   ["Legacy",mythLegacy,6];
-    var tierFreshman  =   ["Freshman",mythFresh,10];
-    var tierGuest     =   ["Guest",mythGuest,11];
-    var tierHonorary  =   ["Honorary",mythHonorary,9];
-    var tierLurker    =   ["Lurker",mythLurker,8];
-    var tierGold      =   ["Gold",mythGold,3];
-    var tierGhost     =   ["Ghost",mythGhost,];
-    var tierPrime     =   ["Prime",mythPrime,1];
+    var tierUnicorn   =   ["Unicorn",mythUnicorn];
+    var tierPlatinum  =   ["Platinum",mythPlatinum];
+    var tierSilver    =   ["Silver",mythSilver];
+    var tierLegacy    =   ["Legacy",mythLegacy];
+    var tierFreshman  =   ["Freshman",mythFresh];
+    var tierGuest     =   ["Guest",mythGuest];
+    var tierHonorary  =   ["Honorary",mythHonorary];
+    var tierLurker    =   ["Lurker",mythLurker];
+    var tierGold      =   ["Gold",mythGold];
+    var tierGhost     =   ["Ghost",mythGhost];
+    var tierPrime     =   ["Prime",mythPrime];
  
 
 
 
 //Badges    
-    var jogueiro = createBadge('Jogueiros: Sempre presentes nas jogatinas mÃ­ticas', 'joy');
-    var hangouteiro = createBadge('Hangouteiro: Sempre marcando presenÃ§a nos Hangouts!', 'hang');
+    var jogueiro = createBadge('Jogueiros: Sempre presentes nas jogatinas míticas', 'joy');
+    var hangouteiro = createBadge('Hangouteiro: Sempre marcando presença nos Hangouts!', 'hang');
     var desenheiro = createBadge('Desenheiro: Depictador dos melhores momentos.', 'pen');
-    var vassouraWielder = createBadge('Vassoura Wielder: JÃ¡ vassourou alguÃ©m em uma situaÃ§Ã£o realmente necessÃ¡ria', 'broom');
+    var vassouraWielder = createBadge('Vassoura Wielder: Já vassourou alguém em uma situação realmente necessária', 'broom');
     var _3dsPlay = createBadge('3DS Teamplay: Jogou com mitadores via Nintendo 3DS', '3ds');
     var defiance = createBadge('Defiance: Sem medo de umas vassouradas', 'defiance');
     var pipoqueiro = createBadge('Arthur exclusive \'Pipoqueiro Badge\'', 'pop');
@@ -56,39 +55,39 @@
    
    
     var lendsClub = createBadge('Lends Club: Board Games com Mitadores', 'lends');
-    var badgeWhore = createBadge('Badge Whore - FaÃ§o tudo por badges!', 'badwho');
-    var kanaFest = createBadge('Kanafester - GÃªnios do Pseudo-JaponÃªs', 'kana');
+    var badgeWhore = createBadge('Badge Whore - Faço tudo por badges!', 'badwho');
+    var kanaFest = createBadge('Kanafester - Gênios do Pseudo-Japonês', 'kana');
     var cupidRab = createBadge('Coelho exclusive \'Official Cupid\' Badge','cupid');
-    var pauHd = createBadge('AragÃ£o exclusive \'Pau em HD\' Badge','pauhd');
+    var pauHd = createBadge('Aragão exclusive \'Pau em HD\' Badge','pauhd');
     var likeBomb = createBadge('Like Bomb - Top da balada','likebomb');
     var vidalGt = createBadge('Vidal exclusive \'Greentext\' Badge','greentxt');
-    var vidalFdp = createBadge('Vidal exclusive \'FilhadaputÃ³polis\' Badge','catan');
+    var vidalFdp = createBadge('Vidal exclusive \'Filhadaputópolis\' Badge','catan');
     var miauCaralho = createBadge('Ana exclusive \'MIAO CARALHO\' Badge');
-    var ajudeiro = createBadge('Ajudeiro - Ele nÃ£o Ã© o Luciano, mas tambÃ©m ajuda pra caramba!');
-    var redCoder = createBadge('Red Coder: Coder LendÃ¡rio','redcoder');
-    var goldenPen = createBadge('Golden Pen: Desenheiro LendÃ¡rio');
+    var ajudeiro = createBadge('Ajudeiro - Ele não é o Luciano, mas também ajuda pra caramba!');
+    var redCoder = createBadge('Red Coder: Coder Lendário','redcoder');
+    var goldenPen = createBadge('Golden Pen: Desenheiro Lendário');
     var tunelaa = createBadge('Tumeo exclusive \'???\' Badge');
     var wataZomb = createBadge('Watanabe Exclusive \'Japanese Horror Movie\' Badge','akuma');
-    var preHistoric = createBadge('Mitador JurÃ¡ssico - Os Mitadores PrÃ©-HistÃ³ricos','jurassic');
+    var preHistoric = createBadge('Mitador Jurássico - Os Mitadores Pré-Históricos','jurassic');
     var musicist = createBadge('Musiqueiro','music');
     var keidiPint = createBadge('Keidi exclusive \'Pinteiro\' Badge','print');
-    var merito = createBadge('Mitador por mÃ©rito - Admin/Community pick','merito');
+    var merito = createBadge('Mitador por mérito - Admin/Community pick','merito');
     var matricialPrint = createBadge('Vidal exclusive \'Impressora Matricial\' Badge','printer');
     var happyDoor = createBadge('Karen exclusive \'Porta Feliz\' Badge','hpydoa');
     var satanicat = createBadge('Matsuna exclusive \'Satanicat\' Badge','satanicat');
-    var karaoke = createBadge('KaraokÃª Mitador: Eu fui!','karaoke');
+    var karaoke = createBadge('Karaokê Mitador: Eu fui!','karaoke');
     var nennys = createBadge('Mitador Nennys: o mais novo','nennys');
-    var elder = createBadge('Mitador AnciÃ£o: o mais velho','ancient');
+    var elder = createBadge('Mitador Ancião: o mais velho','ancient');
     var mariEx = createBadge('Pall exclusive \'!!!\' Badge','excla');
     var fuQuest = createBadge('Fuquest/OsMitadores the Game: Devteam','fq');
     var radiant = createBadge('Radiant Historia 11k','historia');
 
-    var genesis = createBadge('AnfitriÃ£o de SEGA Genesis','genesis');
-    var n64 = createBadge('AnfitriÃ£o de Nintendo 64','n64');
-    var ps2 = createBadge('AnfitriÃ£o de Playstation 2','ps2');
-    var ps3 = createBadge('AnfitriÃ£o de Playstation 3','ps3');
-    var ps4 = createBadge('AnfitriÃ£o de Playstation 4','ps4');
-    var wii = createBadge('AnfitriÃ£o de Nintendo Wii','wii');
+    var genesis = createBadge('Anfitrião de SEGA Genesis','genesis');
+    var n64 = createBadge('Anfitrião de Nintendo 64','n64');
+    var ps2 = createBadge('Anfitrião de Playstation 2','ps2');
+    var ps3 = createBadge('Anfitrião de Playstation 3','ps3');
+    var ps4 = createBadge('Anfitrião de Playstation 4','ps4');
+    var wii = createBadge('Anfitrião de Nintendo Wii','wii');
 
     var nonethless = createBadge('nodesc');
     
@@ -98,7 +97,7 @@
     var cGors = createBadge('The Bullet Head: Gors Crest', 'gors');
     var cAna = createBadge('Yomiell Hankerchief: Ana Crest', 'yomi');
     var cPall = createBadge('The Kawaii Stick Pall Crest', 'pall');
-    var cClaro = createBadge('Com o Claro vocÃª fala ilimitado! Claro Crest', 'claro');
+    var cClaro = createBadge('Com o Claro você fala ilimitado! Claro Crest', 'claro');
     var cHerbe = createBadge('A Erva da Alegria: Herbe Crest', 'herb');
     var cCesar = createBadge('The Rockstar Sneakers: Rasec Crest', 'rasec');
 
@@ -106,16 +105,16 @@
 
 
 /*
-*   FUNÃ‡Ã•ES
+*   FUNÇÕES
 */
 
 
 function createBadge(title, imgName) {
 /*
-*   DescriÃ§Ã£o: FunÃ§Ã£o que simplifica a criaÃ§Ã£o das variÃ¡veis
-*   das badges a partir do tÃ­tulo da imagem e nome do arquivo
-*   sem a extensÃ£o.
-*   Args: 1. TÃ­tulo da imagem (string); 2. Nome do arquivo (string).
+*   Descrição: Função que simplifica a criação das variáveis
+*   das badges a partir do título da imagem e nome do arquivo
+*   sem a extensão.
+*   Args: 1. Título da imagem (string); 2. Nome do arquivo (string).
 */  
 
     if (!imgName){
@@ -142,7 +141,7 @@ function createTable(mito) {
     var crest = mito.crest;
 
      if (!mito.crest){
-        crest = ['Este Mitador ainda nÃ£o possui Crest prÃ³pria','none'];
+        crest = ['Este Mitador ainda não possui Crest própria','none'];
     }
 
     return ('<table  class="tg" style="table-layout: margin-bottom: 10px; fixed; width: 100%"> \
@@ -178,3 +177,7 @@ function createTable(mito) {
     
 }
 
+function mitar(MITO){
+    MITO = ('\n ▓▓▓▓▓▓▓▓  ▓▓▓▓▓▓▓▓  ▓▓▓   ▓▓  ▓▓▓▓▓▓▓▓  ▓▓▓▓▓▓▓▓ \n ▓▓    ▓▓  ▓▓        ▓▓▓▓  ▓▓  ▓▓        ▓▓    ▓▓ \n ▓▓    ▓▓  ▓▓        ▓▓▓▓  ▓▓  ▓▓        ▓▓    ▓▓ \n ▓▓▓▓▓▓▓▓  ▓▓▓▓▓▓▓▓  ▓▓ ▓▓ ▓▓  ▓▓▓▓▓▓▓▓  ▓▓    ▓▓ \n ▓▓        ▓▓        ▓▓  ▓▓▓▓        ▓▓  ▓▓    ▓▓ \n ▓▓        ▓▓        ▓▓  ▓▓▓▓        ▓▓  ▓▓    ▓▓ \n ▓▓        ▓▓▓▓▓▓▓▓  ▓▓   ▓▓▓  ▓▓▓▓▓▓▓▓  ▓▓▓▓▓▓▓▓ \n\n\n ▓▓        ▓▓▓▓▓▓▓▓  ▓▓▓▓▓▓▓▓  ▓▓▓▓▓▓▓▓ \n ▓▓        ▓▓    ▓▓  ▓▓        ▓▓    ▓▓ \n ▓▓        ▓▓    ▓▓  ▓▓        ▓▓    ▓▓ \n ▓▓        ▓▓    ▓▓  ▓▓  ▓▓▓▓  ▓▓    ▓▓ \n ▓▓        ▓▓    ▓▓  ▓▓    ▓▓  ▓▓    ▓▓ \n ▓▓        ▓▓    ▓▓  ▓▓    ▓▓  ▓▓    ▓▓ \n ▓▓▓▓▓▓▓▓  ▓▓▓▓▓▓▓▓  ▓▓▓▓▓▓▓▓  ▓▓▓▓▓▓▓▓ \n\n\n ▓▓    ▓▓     ▓▓     ▓▓▓▓▓▓▓▓  ▓▓▓▓▓▓▓▓ \n ▓▓▓  ▓▓▓               ▓▓     ▓▓    ▓▓ \n ▓▓▓▓▓▓▓▓     ▓▓        ▓▓     ▓▓    ▓▓ \n ▓▓ ▓▓ ▓▓     ▓▓        ▓▓     ▓▓    ▓▓ \n ▓▓    ▓▓     ▓▓        ▓▓     ▓▓    ▓▓ \n ▓▓    ▓▓     ▓▓        ▓▓     ▓▓    ▓▓ \n ▓▓    ▓▓     ▓▓        ▓▓     ▓▓▓▓▓▓▓▓ \n ');
+    console.log(MITO);
+}
