3d25db4c9d1d264677bcb64e1f9c8d2ccc891a12
LucasFlicky <lucasflicky@hotmail.com>
2015-06-20 21:44:23 -0700

    Update mythmitos.js

--- mythmitos.js
+++ mythmitos.js
@@ -103,7 +103,7 @@
         nome: 'Camila',
         tagId: 'cpaolucci',
         tagId1: 'cpaolucci1',
-        faceId: '558835807587895',
+        faceId: 'camila.paolucci.3',
         tier: tierFreshman,
         ano: '2015',
         badges: [
